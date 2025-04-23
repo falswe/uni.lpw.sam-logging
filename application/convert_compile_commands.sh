22507a950656cf867cc09b6d573b4002fd7eb0e0
falswe <w.falschlunger@studenti.unitn.it>
2025-04-23 11:21:08 +0200

    native_posix target for standalone test

--- application/convert_compile_commands.sh
+++ application/convert_compile_commands.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jq '[ .[] | .command = (.command | sub("-fno-reorder-functions"; "") | sub("-mfp16-format=ieee"; "") | sub("-fno-defer-pop"; "")) ]' $1 > $2
+jq '[ .[] | .command = (.command | sub("-fno-reorder-functions"; "") | sub("-mfp16-format=ieee"; "") | sub("-fno-defer-pop"; "") | sub("-fno-freestanding"; "")) ]' $1 > $2
